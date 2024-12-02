f1191021c142a2173b2350aefa6cbaeb27c1f8df
srgelber <sgelber@calpoly.edu>
2024-12-01 16:45:00 -0800

    First VADER implementation

---
+++ vader.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('ggplot')
+
+import nltk
+
+# Read in data
+df = pd.read_csv('Reviews.csv')
+#(df.shape)
+df = df.head(500)
+#print(df.shape)
+
+#initial EDA
+"""
+ax = df['Score'].value_counts().sort_index() \
+    .plot(kind='bar',
+          title='Count of Reviews by Stars',
+          figsize=(10, 5))
+ax.set_xlabel('Review Stars')
+plt.show()
+"""
+
+#basic NLTK
+example = df['Text'][50]
+tokens = nltk.word_tokenize(example)
+tagged = nltk.pos_tag(tokens)
+entities = nltk.chunk.ne_chunk(tagged)
+
+#VADER analysis
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm.notebook import tqdm
+
+sia = SentimentIntensityAnalyzer()
+
+result = {}
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    text = row['Text']
+    myid = row['Id']
+    result[myid] = sia.polarity_scores(text)
+
+
+vaders = pd.DataFrame(res).T
+vaders = vaders.reset_index().rename(columns={'index': 'Id'})
+vaders = vaders.merge(df, how='left')
+
+ax = sns.barplot(data=vaders, x='Score', y='compound')
+ax.set_title('Compound Score by Amazon Star Review')
+plt.show()
+
+fig, axs = plt.subplots(1, 3, figsize=(12, 3))
+sns.barplot(data=vaders, x='Score', y='pos', ax=axs[0])
+sns.barplot(data=vaders, x='Score', y='neu', ax=axs[1])
+sns.barplot(data=vaders, x='Score', y='neg', ax=axs[2])
+axs[0].set_title('Positive')
+axs[1].set_title('Neutral')
+axs[2].set_title('Negative')
+plt.tight_layout()
+plt.show()
